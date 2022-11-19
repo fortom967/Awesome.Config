25482b495be703cf12b6bbd32c42ce1d4b684080
TonyTomCat01 <tonytomcat01@gmail.com>
2022-11-19 08:01:04 +0530

    Some Changes

--- ui/panel.lua
+++ ui/panel.lua
@@ -72,57 +72,58 @@ ram = wibox.widget({
 
 progress = constraint(ram, "exact", 125, 125)
 
--- gears.timer({
--- 	timeout = 1,
--- 	call_now = true,
--- 	autostart = true,
--- 	callback = function()
--- 		awful.spawn.easy_async_with_shell("mpstat | awk 'FNR == 4 {print $3}'", function(a)
--- 			cpu.widget.value = tonumber(a)
--- 		end)
--- 		awful.spawn.easy_async_with_shell("df -BM | grep sda | awk '{print $2" .. '" "' .. "$3}'", function(b)
--- 			local words = {}
--- 
--- 			for word in b:gmatch("[0-9]+") do
--- 				table.insert(words, word)
--- 			end
--- 
--- 			fs.widget.max_value = tonumber(words[1])
--- 			fs.widget.value = tonumber(words[2])
--- 		end)
--- 
--- 		awful.spawn.easy_async_with_shell("cat /proc/meminfo | grep MemTotal | awk '{print $2}'", function(r)
--- 			t = tonumber(r)
--- 			awful.spawn.easy_async_with_shell("cat /proc/meminfo | grep MemFree", function(c)
--- 				f = t - tonumber(string.sub(c, string.find(c, "[0-9]+")))
--- 				ram.max_value = t
--- 				ram.value = f
--- 			end)
--- 		end)
--- 
--- 		awful.spawn.easy_async_with_shell("acpi -b | awk '{print $3$4}'", function(out)
--- 			if string.find(out, "Charging") then
--- 				battery.update = not battery.update or false
--- 				if battery.update then
--- 					battery.widget.bar_active_color = "#69EF86"
--- 					battery.widget.handle_color = "#69EF86"
--- 				else
--- 					battery.widget.bar_active_color = "#69BE86"
--- 					battery.widget.handle_color = "#69BE86"
--- 				end
--- 			end
--- 			battery.widget.value = tonumber(string.sub(out, string.find(out, "[0-9]+")))
--- 		end)
--- 
--- 		awful.spawn.easy_async_with_shell("pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'", function(out)
--- 			volume.widget.value = tonumber(string.sub(out, string.find(out, "[0-9]+")))
--- 		end)
--- 
--- 		awful.spawn.easy_async_with_shell("brightnessctl i | grep Current | awk '{print $4}'", function(bright)
--- 			brightness.widget.value = tonumber(string.sub(bright, string.find(bright, "[0-9]+")))
--- 		end)
--- 	end,
--- })
+gears.timer({
+	timeout = 1,
+	call_now = true,
+	autostart = true,
+	callback = function()
+		awful.spawn.easy_async_with_shell("mpstat | awk 'FNR == 4 {print $3}'", function(a)
+			cpu.widget.value = tonumber(a)
+		end)
+
+		awful.spawn.easy_async_with_shell("df -BM | grep sda | awk '{print $2" .. '" "' .. "$3}'", function(b)
+			local words = {}
+
+			for word in b:gmatch("[0-9]+") do
+				table.insert(words, word)
+			end
+
+			fs.widget.max_value = tonumber(words[1])
+			fs.widget.value = tonumber(words[2])
+		end)
+
+		awful.spawn.easy_async_with_shell("cat /proc/meminfo | grep MemTotal | awk '{print $2}'", function(r)
+			t = tonumber(r)
+			awful.spawn.easy_async_with_shell("cat /proc/meminfo | grep MemFree", function(c)
+				f = t - tonumber(string.sub(c, string.find(c, "[0-9]+")))
+				ram.max_value = t
+				ram.value = f
+			end)
+		end)
+
+		awful.spawn.easy_async_with_shell("acpi -b | awk '{print $3$4}'", function(out)
+			if string.find(out, "Charging") then
+				battery.update = not battery.update or false
+				if battery.update then
+					battery.widget.bar_active_color = "#69EF86"
+					battery.widget.handle_color = "#69EF86"
+				else
+					battery.widget.bar_active_color = "#69BE86"
+					battery.widget.handle_color = "#69BE86"
+				end
+			end
+			battery.widget.value = tonumber(string.sub(out, string.find(out, "[0-9]+")))
+		end)
+
+		-- awful.spawn.easy_async_with_shell("pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'", function(out)
+		-- 	volume.widget.value = tonumber(string.sub(out, string.find(out, "[0-9]+")))
+		-- end)
+
+		awful.spawn.easy_async_with_shell("brightnessctl i | grep Current | awk '{print $4}'", function(bright)
+			brightness.widget.value = tonumber(string.sub(bright, string.find(bright, "[0-9]+")))
+		end)
+	end,
+})
 
 sliders = constraint({
 	layout = layout.align.vertical,
